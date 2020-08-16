7c62a5e40d7c3ba329b9f3065c8bbfd949795ecd
Musashi Sakamoto <1292602b@gmail.com>
2020-08-16 15:49:12 -0700

    wip flex

--- src/components/VideoNav/index.js
+++ src/components/VideoNav/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
-import { VideoDiv, Container } from './styles'
+import { VideoDiv, Container, Item, FlexContainer } from './styles'
 
 const VideoNav = () => {
   return (
     <Container>
+      <FlexContainer>
+        <Item></Item>
+        <Item></Item>
+      </FlexContainer>
+      <FlexContainer>
+        <Item></Item>
+        <Item></Item>
+        <Item></Item>
+      </FlexContainer>
       <VideoDiv>
         <iframe
           id="player"
@@ -12,7 +21,7 @@ const VideoNav = () => {
           allowFullScreen="1"
           width="100%"
           height="100%"
-          src="https://www.youtube.com/embed/JQbjS0_ZfJ0?autoplay=1&amp;controls=0&amp;loop=1&amp;showinfo=0&amp;autohide=1&amp;rel=0&amp;mute=1&amp;index=4&amp;enablejsapi=1&amp;widgetid=1&amp;modestbranding=1"
+          src="https://www.youtube.com/embed/ue5oHmUGiMM?autoplay=1&amp;controls=0&amp;loop=1&amp;showinfo=0&amp;autohide=1&amp;rel=0&amp;mute=1&amp;index=4&amp;enablejsapi=1&amp;widgetid=1&amp;modestbranding=1"
           frameborder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
--- src/components/VideoNav/styles.js
+++ src/components/VideoNav/styles.js
@@ -7,11 +7,27 @@ export const Container = styled.div`
   position: relative;
   height: 700px;
 `
+
+export const FlexContainer = styled.div`
+  display: flex;
+`
+
+export const Item = styled.div`
+  opacity: 0;
+  height: 350px;
+  width: 100%;
+  background-color: white;
+  z-index: 0;
+  &:hover {
+    opacity: 1;
+  }
+`
+
 export const VideoDiv = styled.div`
   position: absolute;
   top: -60px;
   left: 0;
-  z-index: -2000;
+  z-index: -9999;
   width: 100%;
   height: 100%;
 `
