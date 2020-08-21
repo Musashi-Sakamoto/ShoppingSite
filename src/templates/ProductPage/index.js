3d4bb447b93e16b1a1e283e0744eb18f252c1369
Musashi Sakamoto <1292602b@gmail.com>
2020-08-21 10:06:58 -0700

    carousel added

--- src/templates/ProductPage/index.js
+++ src/templates/ProductPage/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-
+import 'react-responsive-carousel/lib/styles/carousel.min.css' // requires a loader
+import { Carousel } from 'react-responsive-carousel'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
 import SEO from '~/components/seo'
 import ProductForm from '~/components/ProductForm'
 import { Img, Container } from '~/utils/styles'
@@ -14,19 +17,92 @@ import {
 
 const ProductPage = ({ data }) => {
   const product = data.shopifyProduct
+  const arrowStyles = {
+    position: 'absolute',
+    zIndex: 2,
+    top: 'calc(50% - 15px)',
+    width: 30,
+    height: 30,
+    cursor: 'pointer',
+    backgroundColor: 'transparent',
+    border: 'none',
+    outline: 'none',
+  }
+  const indicatorStyles = {
+    background: '#aaa',
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    display: 'inline-block',
+    margin: '0 8px',
+  }
   return (
     <>
       <SEO title={product.title} description={product.description} />
       <Container>
         <TwoColumnGrid>
           <GridLeft>
-            {product.images.map(image => (
-              <Img
-                fluid={image.localFile.childImageSharp.fluid}
-                key={image.id}
-                alt={product.title}
-              />
-            ))}
+            <Carousel
+              renderArrowPrev={(onClickHandler, hasPrev, label) =>
+                hasPrev && (
+                  <button
+                    type="button"
+                    onClick={onClickHandler}
+                    title={label}
+                    style={{ ...arrowStyles, left: 15 }}
+                  >
+                    <FontAwesomeIcon icon={faAngleLeft} />
+                  </button>
+                )
+              }
+              renderArrowNext={(onClickHandler, hasNext, label) =>
+                hasNext && (
+                  <button
+                    type="button"
+                    onClick={onClickHandler}
+                    title={label}
+                    style={{ ...arrowStyles, right: 15 }}
+                  >
+                    <FontAwesomeIcon icon={faAngleRight} />
+                  </button>
+                )
+              }
+              renderIndicator={(onClickHandler, isSelected, index, label) => {
+                if (isSelected) {
+                  return (
+                    <li
+                      style={{ ...indicatorStyles, background: '#000' }}
+                      aria-label={`Selected: ${label} ${index + 1}`}
+                      title={`Selected: ${label} ${index + 1}`}
+                    />
+                  )
+                }
+                return (
+                  <li
+                    style={indicatorStyles}
+                    onClick={onClickHandler}
+                    onKeyDown={onClickHandler}
+                    value={index}
+                    key={index}
+                    role="button"
+                    tabIndex={0}
+                    title={`${label} ${index + 1}`}
+                    aria-label={`${label} ${index + 1}`}
+                  />
+                )
+              }}
+              infiniteLoop
+              swipeable
+              showStatus={false}
+            >
+              {product.images.map(image => (
+                <Img
+                  fluid={image.localFile.childImageSharp.fluid}
+                  key={image.id}
+                  alt={product.title}
+                />
+              ))}
+            </Carousel>
           </GridLeft>
           <GridRight>
             <ProductTitle>{product.title}</ProductTitle>
