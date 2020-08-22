820ccca108024527758fdbb7bb1f46c1fe5da958
Musashi Sakamoto <1292602b@gmail.com>
2020-08-21 21:30:34 -0700

    item cell 完成

--- src/components/Cart/LineItem/styles.js
+++ src/components/Cart/LineItem/styles.js
@@ -1,9 +1,60 @@
 import styled from '@emotion/styled'
 
 export const Wrapper = styled.div`
+  border-top: 1px solid lightgrey;
+  border-bottom: 1px solid lightgrey;
   display: flex;
   justify-content: space-between;
   align-items: center;
   flex-wrap: wrap;
-  padding: 2rem 0 2rem 0;
+`
+
+export const Img = styled.img`
+  width: 150px;
+  height: auto;
+  margin-right: 8px;
+`
+
+export const InfoDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  flex-grow: 1;
+`
+
+export const QuantityInput = styled.input`
+  font-size: 0.8rem;
+  text-align: center;
+  outline: none;
+  border: 2px solid black;
+  height: 50px;
+  width: 60px;
+  &::-webkit-outer-spin-button,
+  &::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+`
+
+export const ProductTitle = styled.p`
+  text-transform: uppercase;
+  font-size: 0.8rem;
+`
+
+export const Size = styled.p`
+  margin-top: 8px;
+  color: lightgrey;
+  font-size: 0.8rem;
+`
+
+export const Price = styled.p`
+  font-size: 0.7rem;
+`
+
+export const RemoveButton = styled.button`
+  outline: none;
+  background-color: white;
+  border: none;
+  margin-left: 8px;
+  cursor: pointer;
 `
