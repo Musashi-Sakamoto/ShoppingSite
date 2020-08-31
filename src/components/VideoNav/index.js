629a635986de9656312ff7b6ccd592a05db5323a
Musashi Sakamoto <1292602b@gmail.com>
2020-08-31 10:06:02 -0700

    closes #17

--- src/components/VideoNav/index.js
+++ src/components/VideoNav/index.js
@@ -95,14 +95,13 @@ const VideoNav = () => {
       {!isSmall && (
         <VideoDiv>
           <iframe
-            title="youtube"
-            id="player"
-            allowFullScreen="1"
+            title="front page"
             width="100%"
             height="100%"
-            src="https://www.youtube.com/embed/ue5oHmUGiMM?autoplay=1&amp;controls=0&amp;loop=1&amp;showinfo=0&amp;autohide=1&amp;rel=0&amp;mute=1&amp;index=4&amp;enablejsapi=1&amp;widgetid=1&amp;modestbranding=1&amp;playlist=ue5oHmUGiMM"
-            frameBorder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            src="https://player.vimeo.com/video/453346847?autoplay=1&loop=1&title=0&byline=0&portrait=0&muted=1"
+            frameborder="0"
+            allow="autoplay; fullscreen"
+            allowfullscreen
           ></iframe>
         </VideoDiv>
       )}
