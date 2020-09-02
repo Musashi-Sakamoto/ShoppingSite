dddc30bf1d93225205222ae83d93ad7653f28480
Musashi Sakamoto <1292602b@gmail.com>
2020-09-02 09:50:56 -0700

    video size issue fix

--- src/templates/CollectionPage/styles.js
+++ src/templates/CollectionPage/styles.js
@@ -1,14 +1,19 @@
 import styled from '@emotion/styled'
-import { Img } from '~/utils/styles'
+import { Img, breakpoints } from '~/utils/styles'
 
 export const CollectionImage = styled(Img)``
 
 export const Banner = styled.div`
+  position: relative;
+  padding-bottom: 56.25%; /* 16:9 */
   & > * {
-    width: 100%;
-  }
-  & > iframe {
     z-index: -9999;
     pointer-events: none;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: black;
   }
 `
